f0afc334abbf901da9b007f0874a9d6bfa17700e
Epinephrine0 <86330156+Epinephrine0@users.noreply.github.com>
2021-11-20 18:31:44 +0300

    Starting screen added.

--- Project/src/project/GUI.java
+++ Project/src/project/GUI.java
@@ -1,8 +1,13 @@
 package project;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class GUI extends Application{
@@ -10,15 +15,37 @@ public class GUI extends Application{
 	@Override
 	public void start(Stage primaryStage)  {
 		Pane startScreen = new Pane();
-		Scene scene = new Scene(startScreen, 400, 400);
-		primaryStage.setTitle("Project"); // Set the stage title
-		primaryStage.setScene(scene); // Place the scene in the stage
-		primaryStage.show(); // Display the stage
+		Label registrationLabel = new Label("Registration System");
+		
+		registrationLabel.setFont(Font.font ("arial", 40));
+		registrationLabel.setLayoutX(240);
+		registrationLabel.setLayoutY(200);
+
+		HBox hbox = new HBox(10);
+		Button viewCourse = new Button("View Course");
+		Button viewStudentDetails = new Button("View Student Details");
+		Button save = new Button("Save");
+		hbox.getChildren().addAll(viewCourse,viewStudentDetails,save);
+		hbox.setLayoutX(280);
+		hbox.setLayoutY(550);
+		
+
+
+		
+		startScreen.getChildren().add(registrationLabel);
+		startScreen.getChildren().add(hbox);
+		
+		
+		
+		Scene scene = new Scene(startScreen, 850, 600);
+		primaryStage.setTitle("Project"); 
+		primaryStage.setScene(scene); 
+		primaryStage.show(); 
 		
 	}
 
 	public static void main(String[] args) {
-		CommonClass.loadBinaryData();
+		//CommonClass.loadBinaryData();
 		Application.launch(args);
 	}
 }
